3279cbaf2cace0d7c11940c07dfee43ff8cd046d
tusar1986 <tusartkb@gmail.com>
2020-07-15 11:32:49 +0530

    Changes in LaunchChromeBrowser

--- MySeleniumProject/src/com/selenium/LaunchChromeBrowser.java
+++ MySeleniumProject/src/com/selenium/LaunchChromeBrowser.java
@@ -5,16 +5,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LaunchChromeBrowser {
 
 	public static void main(String[] args) {
-		
-		System.setProperty("webdriver.chrome.driver","E:\\Selenium Ravilella\\SeleniumWebdrivers\\chromedriver.exe");
-		ChromeDriver driver=new ChromeDriver();
+
+		System.setProperty("webdriver.chrome.driver", "E:\\Selenium Ravilella\\SeleniumWebdrivers\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
 		driver.close();
-		System.setProperty("webdriver.geco.driver","E:\\Selenium Ravilella\\SeleniumWebdrivers\\geckodriver.exe\\");
-		ChromeDriver driver1=new ChromeDriver();
-//		driver1.get("https://www.amazon.in");
-		driver1.navigate().to("https://www.amazon.in");
-        driver1.close();
+		System.setProperty("webdriver.geco.driver", "E:\\Selenium Ravilella\\SeleniumWebdrivers\\geckodriver.exe\\");
+		ChromeDriver driver1 = new ChromeDriver();
+		driver1.get("https://www.amazon.in");
+		// driver1.navigate().to("https://www.amazon.in");
+		driver1.close();
 	}
 
 }
