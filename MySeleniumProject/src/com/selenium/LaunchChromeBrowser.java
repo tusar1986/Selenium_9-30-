e55ebd7f8e63e4e52250eb29662720e9e4ed3560
tusar1986 <tusartkb@gmail.com>
2020-07-16 09:47:36 +0530

    Code changes added

--- MySeleniumProject/src/com/selenium/LaunchChromeBrowser.java
+++ MySeleniumProject/src/com/selenium/LaunchChromeBrowser.java
@@ -1,6 +1,7 @@
 package com.selenium;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LaunchChromeBrowser {
 
@@ -9,11 +10,13 @@ public class LaunchChromeBrowser {
 		System.setProperty("webdriver.chrome.driver", "E:\\Selenium Ravilella\\SeleniumWebdrivers\\chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
 		driver.close();
-		System.setProperty("webdriver.geco.driver", "E:\\Selenium Ravilella\\SeleniumWebdrivers\\geckodriver.exe\\");
-		ChromeDriver driver1 = new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver", "E:\\Selenium Ravilella\\SeleniumWebdrivers\\geckodriver.exe\\");
+		FirefoxDriver driver1 = new FirefoxDriver();
 		driver1.get("https://www.amazon.in");
 		// driver1.navigate().to("https://www.amazon.in");
+		driver.manage().window().maximize();
 		driver1.close();
 	}
 
