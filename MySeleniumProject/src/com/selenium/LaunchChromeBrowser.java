9c652887a538cc9007193083bff526382181e4b9
tusar1986 <tusartkb@gmail.com>
2020-07-16 10:45:04 +0530

    New java program files are added

--- MySeleniumProject/src/com/selenium/LaunchChromeBrowser.java
+++ MySeleniumProject/src/com/selenium/LaunchChromeBrowser.java
@@ -1,23 +1,24 @@
 package com.selenium;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LaunchChromeBrowser {
 
 	public static void main(String[] args) {
-
+		WebDriver driver;
 		System.setProperty("webdriver.chrome.driver", "E:\\Selenium Ravilella\\SeleniumWebdrivers\\chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.get("https://www.google.com");
 		driver.manage().window().maximize();
 		driver.close();
 		System.setProperty("webdriver.gecko.driver", "E:\\Selenium Ravilella\\SeleniumWebdrivers\\geckodriver.exe\\");
-		FirefoxDriver driver1 = new FirefoxDriver();
-		driver1.get("https://www.amazon.in");
+		driver = new FirefoxDriver();
+		driver.get("https://www.amazon.in");
 		// driver1.navigate().to("https://www.amazon.in");
-		driver.manage().window().maximize();
-		driver1.close();
+		driver.close();
+	
 	}
 
 }
